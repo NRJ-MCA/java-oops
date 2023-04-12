21d8a8fbdce6cf15361ad1c8a11d16805bd789ed
NRJ-MCA <122427038+NRJ-MCA@users.noreply.github.com>
2023-04-12 00:08:18 -0700

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sum
+{
+public static void main(string []args)
+{
+ int a,b,n=0;
+System.out.println("enter the number");
+sum=a+b;
+System.out.println("sum =",sum);
+}
+}
